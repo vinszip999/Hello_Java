49a421029b2c9629a14fb87c1b26b32573142e1e
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-05 22:51:59 +0900

    part1.자바 시작하기(주석문)

--- Java/src/javaStudy/HelloWorld.java
+++ Java/src/javaStudy/HelloWorld.java
@@ -1,9 +1,18 @@
 package javaStudy;
 
 public class HelloWorld {
-
+//	프로그램의 시작점.
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+//		System.out.println("test");
+/*		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");	*/
+		
 
 	} // end of main
 
