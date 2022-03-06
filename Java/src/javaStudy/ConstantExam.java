ae105bcfd9ba1c6babe421de70ac9f68acd4b88e
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-06 20:06:51 +0900

    part2.변수와 계산(상수)

---
+++ Java/src/javaStudy/ConstantExam.java
@@ -0,0 +1,26 @@
+package javaStudy;
+
+public class ConstantExam {
+
+	public static void main(String[] args) {
+		int i;
+		i = 10;
+		i = 5;
+		
+		final int J;
+		J = 10;
+		
+//		J = 5; // 상수는 다시 초기화 불가능
+		
+		double circleArea;
+		final double PI = 3.14159;
+		circleArea = 3 * 3 * PI;
+		
+		final int OIL_PRICE = 1450;
+		
+		int totalPrice = 50 * OIL_PRICE;
+		
+
+	} // end of main
+
+} // end of class
