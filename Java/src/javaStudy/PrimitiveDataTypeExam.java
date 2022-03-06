78edcb96c1118fe251657a6c540eb8eb6ccfc821
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-06 20:22:58 +0900

    part2.변수와 계산(기본형 타입)

---
+++ Java/src/javaStudy/PrimitiveDataTypeExam.java
@@ -0,0 +1,22 @@
+package javaStudy;
+
+public class PrimitiveDataTypeExam {
+
+	public static void main(String[] args) {
+		boolean isFun = false; // true값과 false값만 가짐
+		System.out.println(isFun);
+
+		char c = 'f'; // 문자 한글자 표현
+		
+		int x = 59;
+		
+		long bing = 3456789L; // 좀 더 큰 정수값 표현
+		
+		float f = 32.4F; // 실수 표현
+		
+		double d = 345678.8; // 좀 더 큰 실수값 표현
+		
+		
+	} // end of main
+
+} // end of class
